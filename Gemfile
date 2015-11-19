f491c0e223da173d46d787a58394f12732d0958d
knopfler81 <knopfler42@yahoo.fr>
2015-11-19 16:22:36 +0100

    add form

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-
+gem 'haml'
 gem 'devise'
 
 gem 'simple_form', '~> 3.2'
--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,5 +1,44 @@
 class PartiesController < ApplicationController
+  before_action :find_party, only: [:show, :edit, :update, :destroy]
+
   def index
+    @parties = Party.all.order("created_at DESC")
   end
-  
+
+  def show
+  end
+
+  def new
+    @party = Party.new
+  end
+
+  def create
+    @party = Party.new(parties_params)
+
+    if @party.save
+      redirect_to @party
+    else
+      render 'new'
+    end
+  end
+
+  def edit 
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def parties_params
+    params.require(:party).permit(:avatar, :titre, :categorie, :prix, :date, :heure, :description, :nombreparticipants)
+  end
+
+  def find_party
+    @party = Party.find(params[:id])
+  end
+
 end

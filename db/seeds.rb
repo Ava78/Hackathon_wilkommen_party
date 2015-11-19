f844ae7da5efe0be43c6056c707e78e6be02bbb1
knopfler81 <knopfler42@yahoo.fr>
2015-11-19 13:48:37 +0100

    add images

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.create([{ email: 'guest@wcs.com',
+              password: 'jecode4laloupe',
+              name: 'Guest',
+              first_name: 'Bulma',
+              phone: '0123456789',
+              adress: '6 place jules nadi',
+              ville: 'Narbonne',
+              age: 29,
+              genre:'homme',
+              poids: 70,
+              taille: 175,
+              pseudo: 'Adam',
+              niveau: 'Debutant',
+              centre: true,
+         
+  }])
+
+Centre.create([{
+  numero_siret: 112358,
+  numero_ffe: 'fibo011',
+  name: 'hihihi equitation',
+  user_id: 4
+}])

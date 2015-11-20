f2a460491a2a94deb4189f92f4c8cce4ba1b45b3
knopfler81 <knopfler42@yahoo.fr>
2015-11-20 03:11:48 +0100

    add table reservations

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
 
-  devise_for :users
+devise_for :users
 resources:parties
 root "parties#index"
 resources:users
+resource:reservations
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151120013204) do
+ActiveRecord::Schema.define(version: 20151120020237) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -33,6 +33,16 @@ ActiveRecord::Schema.define(version: 20151120013204) do
     t.string   "adresse"
     t.integer  "categorie_id"
     t.string   "url"
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.integer  "nombre_personnes"
+    t.decimal  "prix_total"
+    t.boolean  "paiement"
+    t.integer  "user_id"
+    t.integer  "party_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
   create_table "users", force: :cascade do |t|
